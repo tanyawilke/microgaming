8ff2e71e8ad1f248d098f87bbcc7f042ffd91cbf
TanyaWilke <tanya.wilke@gmail.com>
2019-04-13 11:45:46 +0200

    New functionality
    Application setup + fileupload integration

---
+++ Controllers/HomeController - Copy.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.Mvc;
+using UploadMultipleFilesInMVC.Helpers;
+
+namespace UploadMultipleFilesInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home  
+        public ActionResult UploadFiles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(HttpPostedFileBase[] files)
+        {
+            TempData["notice"] = "Files saved";
+            bool fileIsTooLarge = false;
+            bool fileExtensionInvalid = false;
+
+            //Ensure model state is valid  
+            if (ModelState.IsValid)
+            {   //iterating through multiple file collection   
+                foreach (HttpPostedFileBase file in files)
+                {
+                    //Checking file is available to save.  
+                    if (file != null)
+                    {
+                        var checkextension = new[] { Path.GetExtension(file.FileName).ToLower() };
+
+                        var allowedFileExtentions = new AllowedFileExtensionsHelper();
+                        var maximumAttachmentSize = new AllowedFileSizeHelper();
+
+                        // Todo one entrypoint into size check
+
+                        if (!allowedFileExtentions.FileExtentionAllowed(checkextension))
+                        {
+                            fileExtensionInvalid = true;
+                            TempData["notice"] = "Only PDF documents and images (.jpg | .jpeg | .png) may be uploaded.";
+                        }
+
+                        if (maximumAttachmentSize.AllowedFileSize(files.Count(), file.ContentLength))
+                        {
+                            fileIsTooLarge = true;
+                            TempData["notice"] = "A single attachment cannot exceed than 3MB and the total attachment size cannot exceed 15MB.";
+                        }
+
+                        if ((!fileExtensionInvalid) && (!fileIsTooLarge))
+                        {
+                            try
+                            {
+                                var InputFileName = Path.GetFileName(file.FileName);
+                                var ServerSavePath = Path.Combine(Server.MapPath("~/UploadedFiles") + InputFileName);
+
+                                //Save file to server folder  
+                                file.SaveAs(ServerSavePath);
+                                //assigning file uploaded status to ViewBag for showing message to user.  
+                                ViewBag.UploadStatus = files.Count().ToString() + " files uploaded successfully.";
+                            }
+                            catch (FileLoadException fEx)
+                            {
+                                TempData["notice"] = "A single attachment cannot exceed than 3MB and the total attachment size cannot exceed 15MB.";
+                            }
+                            catch (Exception ex)
+                            {
+                                TempData["notice"] = ex.Message.ToString();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Helpers/AllowedFileExtensionsHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Microgaming.Helpers
+{
+    public class AllowedFileExtensionsHelper
+    {
+        private readonly string[] allowedFileExtensions = ConfigurationManager.AppSettings["FileExtensionsAllowed"].Split(',');
+
+        public bool FileExtentionAllowed(string[] fileExtentions)
+        {
+            foreach (var extention in fileExtentions)
+            {
+                if (!allowedFileExtensions.Contains(extention))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
