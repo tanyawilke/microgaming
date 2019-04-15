e8a81d823adf86814802fecfe78757e9472c04f4
TanyaWilke <tanya.wilke@gmail.com>
2019-04-15 18:08:24 +0200

    Update HomeController.cs
    Method removed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -69,65 +69,6 @@ namespace FinanceRequest.Controllers
             return callbackUrl;
         }
 
-        //[HttpPost]
-        //public ActionResult Index([Bind(Include = "Id")] FormCollection requestForm, HttpPostedFileBase[] files)
-        //{
-        //    TempData["notice"] = "File(s) saved.";
-        //    bool fileIsTooLarge = false;
-        //    bool fileExtensionInvalid = false;
-
-        //    //Ensure model state is valid  
-        //    if (ModelState.IsValid)
-        //    {   //iterating through multiple file collection   
-        //        foreach (HttpPostedFileBase file in files)
-        //        {
-        //            //Checking file is available to save.  
-        //            if (file != null)
-        //            {
-        //                var checkextension = new[] { Path.GetExtension(file.FileName).ToLower() };
-
-        //                var allowedFileExtentions = new AllowedFileExtensionsHelper();
-        //                var maximumAttachmentSize = new AllowedFileSizeHelper();
-
-        //                // Todo one entrypoint into size check
-
-        //                if (!allowedFileExtentions.FileExtentionAllowed(checkextension))
-        //                {
-        //                    fileExtensionInvalid = true;
-        //                    TempData["notice"] = "Only PDF documents and images (.jpg | .jpeg | .png) may be uploaded.";
-        //                }
-
-        //                if (maximumAttachmentSize.AllowedFileSize(files.Count(), file.ContentLength))
-        //                {
-        //                    fileIsTooLarge = true;
-        //                    TempData["notice"] = "A single attachment cannot exceed than 3MB and the total attachment size cannot exceed 15MB.";
-        //                }
-
-        //                if ((!fileExtensionInvalid) && (!fileIsTooLarge))
-        //                {
-        //                    try
-        //                    {
-        //                        var InputFileName = Path.GetFileName(file.FileName);
-        //                        var ServerSavePath = Path.Combine(Server.MapPath("/UploadedFiles") + InputFileName);
-
-        //                        //Save file to server folder  
-        //                        file.SaveAs(ServerSavePath);
-        //                        //assigning file uploaded status to ViewBag for showing message to user.  
-        //                        // ViewBag.UploadStatus = files.Count().ToString() + " file(s) uploaded.";
-        //                        TempData["notice"] = files.Count().ToString() + " file(s) uploaded.";
-        //                    }
-        //                    catch (Exception ex)
-        //                    {
-        //                        TempData["notice"] = ex.Message.ToString();
-        //                    }
-        //                }
-        //            }
-        //        }
-        //    }
-
-        //    return View();
-        //}
-
         //[Authorize]
         //// POST: AdsViewAdverts/Create
         //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
